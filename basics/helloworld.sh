417c2f06b23b47f0f5c41a04b2f3740055126abd
Saheel Wagh <saheelwagh@Gmail.com>
2019-05-22 13:52:58 +0530

    created hello world script

---
+++ basics/helloworld.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# bash stands for bourne again shell
+#shell instrcutions can be run directly  in shell prompt
+#or
+#shell scripts can be saved in a file -> chmod +x to give this file permission to execute
+#first line of script is started bh shabang (# followed by !) and then path to intepreter
+#
+echo "Hello World"
+echo " Wakatime now enabled"
