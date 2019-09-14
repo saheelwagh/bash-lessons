93920c2b3684d37999f40cc2d77c517138b45fcd
VRPP CO-ORDINATOR <RAMRAO@VRPPs-MacBook-Air.local>
2019-09-14 20:48:48 +0530

    added fibonacci program

---
+++ os mod 4/14 sept /gcd.sh
@@ -0,0 +1,19 @@
+#shell program to calculate gcd
+echo "Enter 2 numbers with space between"
+read a b
+m = $a
+if[$b -lt $m]
+then 
+m = $b
+fi
+while [$m -ne 0]
+do
+x = 'expr $a%$m'
+y = 'expr $b % $m'
+if[ $x -eq 0 -a $y -eq 0]
+then 
+echo gcd of $a and $b is $m
+break
+fi
+m = 'expr $m -1'
+done
